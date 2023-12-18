98df7279aee69c5dcba47980718cbc4e06ab4fa4
Mathias Brynolf <mathiasbrynolf20@gmail.com>
2023-12-18 21:25:45 +0100

    .

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,25 +1,18 @@
 let posts = document.getElementsByClassName("posts");
 let newPostBtn = document.getElementById("new-form-btn");
 let newPost = document.getElementById("newPost");
+let editBtn = document.getElementsByClassName("editBtn");
 let today = new Date();
 
 function checkPostDate() {
   for (let i = 0; i < posts.length; i++) {
     let post = posts[i];
     let dateStr = post.getAttribute("data-date");
-    console.log("den fins vist:", dateStr);
     let date = new Date(dateStr);
     if (date != null) {
       if (date >= today) {
         post.style.display = "none";
-        console.log("i if");
-      } else {
-        console.log(dateStr);
-        console.log("date in html", date);
-        console.log(today);
       }
-    } else {
-      console.log("data-date är null");
     }
   }
 }
@@ -31,9 +24,19 @@ function showNewPost() {
   }
 }
 
+function editBtnClick(i) {
+  console.log(editBtn[i].innerHTML);
+}
+//set default value for new post
 document.getElementById("post-date").defaultValue = today
   .toISOString()
   .split("T")[0];
+//popup for new post
 newPostBtn.addEventListener("click", showNewPost);
+
+for (var i = 0; i < editBtn.length; i++) {
+  let editButton = editBtn[i];
+  editButton.addEventListener("click", editBtnClick(i));
+}
 
 checkPostDate();
